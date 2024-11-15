9ba408a640d065be52640f50e99312376b284dd6
Kimberly Urge <kimeyahguia@gmail.com>
2024-11-15 08:23:10 +0800

    Add files via upload

---
+++ DBConnect.java
@@ -0,0 +1,31 @@
+package ConnectorDB;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class DBConnect {
+    private static Connection vehicle_rental;
+    private static DBConnect dbconnect;
+    private static String url = "jdbc:mysql://localhost:3306/vehiclerentalsystem";
+    private static String user = "root";
+    private static String password = "";
+    
+    public DBConnect (){
+        vehicle_rental = null;
+        
+        try {
+           vehicle_rental = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+        }
+    }
+    
+    public static DBConnect getBConnect(){
+        if (dbconnect == null){
+            dbconnect = new DBConnect();
+        }
+        return dbconnect;
+    }
+    
+    public static Connection getConnection(){
+        return vehicle_rental;
+    }
+}
